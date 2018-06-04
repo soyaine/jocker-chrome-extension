89ff537676ea62b87729a8932769124ec99d42ac
MarvinDuo <soyaine@outlook.com>
2018-06-04 19:06:10 +0800

    support non-https url, change onclick href to https

--- build/background.js
+++ build/background.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(activeTab)
 {
-    var newURL = "http://web.okjike.com/";
+    var newURL = "https://web.okjike.com/";
     chrome.tabs.create({ url: newURL });
 });
